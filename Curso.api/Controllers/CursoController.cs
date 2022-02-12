0916e8833894f6a7129c850e71b914f724db0780
cielio <cielion@hotmail.com>
2022-02-12 14:28:12 -0300

    Teste de Token Bearer com swagger

--- Curso.api/Controllers/CursoController.cs
+++ Curso.api/Controllers/CursoController.cs
@@ -1,18 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Curso.api.Models.Cursos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using System.Threading.Tasks;
 
 namespace Curso.api.Controllers
 {
     [Route("api/v1/cursos")]
     [ApiController]
+    [Authorize]
     public class CursoController : Controller
     {
-        public IActionResult Index()
+        /// <summary>
+        /// Este serviço permite cadastrar curso para o usuário autenticado.
+        /// </summary>
+        /// <returns>Retorna status 201 e dados do curso do usuário</returns>
+        [SwaggerResponse(statusCode: 201, description: "Sucesso ao Cadastrar um curso", Type = typeof(CursoViewModelOutput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpPost]
+        public async Task<IActionResult> Post(CursoViewModelInput cursoViewModelInput)
         {
-            return View();
+
+
+            return Created("", cursoViewModelInput);
+        }
+
+        /// <summary>
+        /// Este serviço permite obter todos os cursos ativos do usuário.
+        /// </summary>
+        /// <returns>Retorna status ok e dados do curso do usuário</returns>
+        [SwaggerResponse(statusCode: 200, description: "Sucesso ao obter os cursos", Type = typeof(CursoViewModelOutput))]
+        [SwaggerResponse(statusCode: 401, description: "Não autorizado")]
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
         }
     }
 }
--- Curso.api/Controllers/UsuarioController.cs
+++ Curso.api/Controllers/UsuarioController.cs
@@ -3,8 +3,13 @@ using Curso.api.Filters;
 using Curso.api.Models;
 using Curso.api.Models.Usuarios;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 
 namespace Curso.api.Controllers
 {
@@ -26,7 +31,35 @@ namespace Curso.api.Controllers
         [ValidaçaoModelStateCustomizado]
         public IActionResult Logar(LoginViewModelInput loginViewModelInput)
         {
-                return Ok(loginViewModelInput); 
+            var usuarioViewModelOutput = new UsuarioViewModelOutput
+            {
+                Codigo = 1,
+                Login = "cielio",
+                Email = "cielion@gmail.com"
+            };
+
+            var secret = Encoding.ASCII.GetBytes("MzfsT&d9gprP>!9$Es(X!5g@;ef!5sbk:jH\\2.}8ZP'qY#7");
+            var symemtricSecurityKey = new SymmetricSecurityKey(secret);
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, usuarioViewModelOutput.Codigo.ToString()),
+                    new Claim(ClaimTypes.Name, usuarioViewModelOutput.Login),
+                    new Claim(ClaimTypes.Email, usuarioViewModelOutput.Email),
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(symemtricSecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var jwtSecurityTokemHandler = new JwtSecurityTokenHandler();
+            var tokenGenerated = jwtSecurityTokemHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokemHandler.WriteToken(tokenGenerated);
+
+            return Ok(new
+            {
+                Token = token,
+                Usuario = usuarioViewModelOutput.Login
+            }); 
         }
 
         [HttpPost]
